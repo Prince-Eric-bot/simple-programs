f221da796ca09dc6c5a7f9cc8d3c15ed421319c2
Prince Eric <princeeric848@gmail.com>
2025-10-16 15:17:06 -0400

    Add_program_to_print_first_10_multiples_of_7
    This program prints the first 10 multiples of 7 using a for loop.

---
+++ 5.c
@@ -0,0 +1,12 @@
+/*Write a C program that prints the first 10 multiples of 7 using a for loop.
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    for(i = 1;i<=10;i++){
+     printf("%d\n",7 * i);   
+    }
+    return 0;
+}
